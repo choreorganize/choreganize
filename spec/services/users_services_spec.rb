03e716fdcdc1192f172bf59c0519386899da8b46
Zach T <ztrokey@gmail.com>
2021-07-30 17:07:26 -0600

    udpate create test

--- spec/services/users_services_spec.rb
+++ spec/services/users_services_spec.rb
@@ -4,37 +4,38 @@ RSpec.describe 'user service' do
   describe 'create_or_find_user' do
     describe ' happy path ' do
       it 'gets us info' do
-        user_info = File.read('spec/fixtures/user_service/user_test.json')
+        json_response = File.read('spec/fixtures/user_service/user_test.json')
 
-        stub_request(:post, 'https://choreganize-api.herokuapp.com/api/v1/roommates?roommate%5Bemail%5D=test_email.com&roommate%5Bgoogle_id%5D=user_id_test&roommate%5Bname%5D=test_name&roommate%5Btoken%5D=token_test')
+        stub_request(:post, 'https://choreganize-api.herokuapp.com/api/v1/roommates')
           .with(
+            body: { '{"roommate":{"name":"Moe deGrasse Tyson","email":"cutthatgrass@ex.com","google_id":"456","token":"1234567890"}}' => nil },
             headers: {
               'Accept' => '*/*',
               'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Content-Length' => '0',
+              'Content-Type' => 'application/x-www-form-urlencoded',
               'User-Agent' => 'Faraday v1.5.1'
             }
           )
-          .to_return(status: 200, body: '', headers: {})
+          .to_return(status: 200, body: json_response, headers: {})
 
         send_to_back_end = { roommate: {
-          name: 'test_name',
-          email: 'test_email.com',
-          google_id: 'user_id_test',
-          token: 'token_test'
+          name: 'Moe deGrasse Tyson',
+          email: 'cutthatgrass@ex.com',
+          google_id: '456',
+          token: '1234567890'
         } }
 
         response = UsersService.create_or_find_user(send_to_back_end)
 
-        expected_response = {
-          name: 'test_name',
-          email: 'test_email.com',
-          google_id: 'user_id_test',
-          token: 'token_test',
-          house_id: 3
-        }
-        # require 'pry'; binding.pry
-        expect(response).to eq(expected_response)
+        expect(response).to be_a(Hash)
+        expect(response[:data]).to have_key(:id)
+        expect(response[:data]).to have_key(:type)
+        expect(response[:data]).to have_key(:attributes)
+        expect(response[:data][:attributes][:name]).to eq('Moe deGrasse Tyson')
+        expect(response[:data][:attributes][:household_id]).to eq('nil')
+        expect(response[:data][:attributes][:email]).to eq('cutthatgrass@ex.com')
+        expect(response[:data][:attributes][:google_id]).to eq('456')
+        expect(response[:data][:attributes][:token]).to eq('1234567890')
       end
     end
     describe ' sad path ' do
