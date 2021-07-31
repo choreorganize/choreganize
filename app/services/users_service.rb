bd33d3a4f035ec6eeb82e30f53ab388b19121798
Zach T <ztrokey@gmail.com>
2021-07-30 19:50:15 -0600

    clean out commented code

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -4,19 +4,8 @@ class UsersService
   end
 
   def self.create_or_find_user(user_info)
-    # require 'pry'; binding.pry
-    # response = connection.post('/api/v1/roommates') do |faraday|
-    #   faraday.params['roommate'] = user_info
-    # end
-    # JSON.parse(response.body, symbolize_names: true)
-    # params = user_info
-    # conn = Faraday.new(url: 'https://choreganize-api.herokuapp.com/', params: params)
-
-    # response = conn.post('/api/v1/roommates')
-
-    # JSON.parse(response.body, symbolize_names: true)
     response = connection.post('/api/v1/roommates', user_info.to_json)
+    data = JSON.parse(response.body, symbolize_names: true)
     # require 'pry'; binding.pry
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
