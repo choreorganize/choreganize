9bcf3cf203907ca32ef58567e4a14b9565779c91
Suzie K <suziekim.dev@gmail.com>
2021-08-03 15:02:09 -0400

    X test

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
   describe "GET /user_show" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/dashboard"
-      
+
       expect(response).to have_http_status(:success)
     end
   end
