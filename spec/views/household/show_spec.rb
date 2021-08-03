b8f123ff3a1fa1ab945a7cc98f5b8213efa1d605
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-08-03 12:48:00 -0600

    set up test frame work for household show

---
+++ spec/views/household/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'houshold show page' do
+
+
+  describe ' unassighned chores section ' do
+    describe 'displays all un assigned chores in household' do
+    end
+    describe 'chores have link to chore show page' do
+    end
+    describe ' chores have a point value displaied ' do
+    end
+    describe '  chores are unassighned' do
+    end
+
+  end
+
+  describe 'roommate list ' do
+    describe 'sees roommates of household  ' do
+    end
+    describe 'each roommate displays chores that they assigned ' do
+    end
+    describe 'displays total points of each roommate ' do
+    end
+    describe '  if no roommates exist' do
+    end
+  end
+
+  describe 'general info ' do
+    describe 'forecast ' do
+    end
+    describe 'house hold id displayed ' do
+    end
+    describe 'todays date ' do
+    end
+
+  end
+
+end
