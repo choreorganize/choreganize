f00ca15c0e6c44a80434964afad16a99d6b82ec5
Suzie K <suziekim.dev@gmail.com>
2021-08-03 16:23:18 -0400

    Test session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-
+  
   def current_user 
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    # @user ||= User.find(session[:user_id]) if session[:user_id]
+    require 'pry'; binding.pry
+    @user ||= GoogleUserFacade.user(session[:user])
+    require 'pry'; binding.pry
   end
 end
