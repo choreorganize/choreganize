576aa0ca289a1fe18a8c3d72b4f3075ca0e15a9a
Emmy Morris <emmymorris1102@gmail.com>
2021-08-03 22:36:10 -0600

    Add more testing for view

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -18,12 +18,21 @@ RSpec.describe "User Dashboard" do
                       })
     @chore2 = Chore.new({
                         id: 2,
-                        task_name: 'Dont Mow',
+                        task_name: 'Clean Dishes',
                         household_id: 123,
-                        description: 'Dont Cut some grass, my friend.',
+                        description: 'Clean some dishes.',
                         weight: 3,
                         frequency: 'daily',
                         outdoor: false
+                      })
+    @chore3 = Chore.new({
+                        id: 3,
+                        task_name: 'Laundry',
+                        household_id: 123,
+                        description: 'Wash some laundry.',
+                        weight: 2,
+                        frequency: 'bimonthly',
+                        outdoor: true
                       })
       @current_user = GoogleUser.new({
                                      google_id: '123',
@@ -32,16 +41,16 @@ RSpec.describe "User Dashboard" do
                                      household_id: 123,
                                      token: 'longgooletokenhere',
                                      incomplete_chores: [@chore1],
-                                     completed_chore: []
+                                     completed_chores: [@chore2]
                                    })
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
   end
   describe "Displays to-do chores" do
     it "shows chores that are incomplete_chores" do
       visit user_dashboard_index_path
-      save_and_open_page
-      expect(page).to have_content("Incomplete Chores")
+      # save_and_open_page
       within(first('.incompleteChores')) do
+        expect(page).to have_content("Incomplete Chores")
         expect(page).to have_content(@chore1.task_name)
         expect(page).to have_content(@chore1.description)
         expect(page).to have_content(@chore1.frequency)
@@ -51,19 +60,29 @@ RSpec.describe "User Dashboard" do
         expect(page).not_to have_content(@chore2.description)
         expect(page).not_to have_content(@chore2.frequency)
         expect(page).not_to have_content(@chore2.weight)
+
+        expect(page).not_to have_content(@chore3.task_name)
+        expect(page).not_to have_content(@chore3.description)
+        expect(page).not_to have_content(@chore3.frequency)
+        expect(page).not_to have_content(@chore3.weight)
       end
 
-      expect(page).to have_content("Complete Chores")
       within(first('.completedChores')) do
+        expect(page).to have_content("Complete Chores")
         expect(page).not_to have_content(@chore1.task_name)
         expect(page).not_to have_content(@chore1.description)
         expect(page).not_to have_content(@chore1.frequency)
         expect(page).not_to have_content(@chore1.weight)
 
-        expect(page).not_to have_content(@chore2.task_name)
-        expect(page).not_to have_content(@chore2.description)
-        expect(page).not_to have_content(@chore2.frequency)
-        expect(page).not_to have_content(@chore2.weight)
+        expect(page).to have_content(@chore2.task_name)
+        expect(page).to have_content(@chore2.description)
+        expect(page).to have_content(@chore2.frequency)
+        expect(page).to have_content(@chore2.weight)
+
+        expect(page).not_to have_content(@chore3.task_name)
+        expect(page).not_to have_content(@chore3.description)
+        expect(page).not_to have_content(@chore3.frequency)
+        expect(page).not_to have_content(@chore3.weight)
       end
     end
   end
