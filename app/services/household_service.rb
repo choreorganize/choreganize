52d4a88fd0e57924b9f98d82780c1b8bc836f892
Zach T <ztrokey@gmail.com>
2021-08-04 11:46:24 -0600

    add weather api call

--- app/services/household_service.rb
+++ app/services/household_service.rb
@@ -7,4 +7,10 @@ class HouseholdService
     response = connection.post('/api/v1/household', household_info)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.weather_data(household_id)
+    response = connection.get("/api/v1/household/#{household_id}")
+    a = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
+  end
 end
