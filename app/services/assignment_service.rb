5eb9982d504b92d86bc866552ec87eee3ad5dc0d
Suzie K <suziekim.dev@gmail.com>
2021-08-04 16:02:05 -0400

    Add update method

--- app/services/assignment_service.rb
+++ app/services/assignment_service.rb
@@ -1,11 +1,15 @@
-class AssignmentsService
-
-  def self.connection
-    Faraday.new('https://choreganize-api.herokuapp.com/')
+class AssignmentService
+  def self.update_assignment(assignment_info)
+    response = connection.patch('/api/v1/assignments', assignment_info)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.create_assignment(assignment_info)
     response = connection.post('/api/v1/assignments', assignment_info)
     JSON.parse(response.body, symbolize_names: true)
+  end
+ 
+  def self.connection
+    Faraday.new('https://choreganize-api.herokuapp.com/')
   end
 end
