38c262b4554281831f8fd68e2e5e96236c6c903c
Suzie K <suziekim.dev@gmail.com>
2021-08-04 16:19:40 -0400

    Add it block

--- spec/services/assignment_service_spec.rb
+++ spec/services/assignment_service_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe "Assignment API" do
   describe 'create an assignment' do
     describe '::create_task', :vcr do
-      
-      user_params = { roommate: {
+      it 'creates amd assignment' do 
+        user_params = { roommate: {
         name: 'Suzie Kim',
         email: 'suziekim.dev@gmail.com',
         google_id: '101278412815195230082',
         token: 'ya29.a0ARrdaM8He2f1lV96cw6s83YS2Ikxtl1g0av-enxXPPK1iEgiy9QtYIgZ6J2DKmHe6VaLlaxgLQX3Tdu8D9Zv52R8eQTnZQG8m1wQ5m_338_6qA9Cbpv9gnAAD1yEPSrj1bcL1O41vCajVE42TsSDC0PS2N7I'
-      } }
+        } }
 
-      roommate = GoogleUserFacade.user(user_params)
+        roommate = GoogleUserFacade.user(user_params)
    
-      attributes = { address: '123 Main Street',
+        attributes = { address: '123 Main Street',
                    city: 'Denver',
                    state: 'CO',
                    id: 123,
@@ -66,9 +66,9 @@ RSpec.describe "Assignment API" do
                                                          conditions: 'light rain',
                                                          icon: '10d' }] } }
 
-      household = Household.new(attributes)
+        household = Household.new(attributes)
       
-      chore = Chore.new({
+        chore = Chore.new({
                             id: 1,
                             task_name: 'Mow',
                             household_id: 123,
@@ -78,15 +78,16 @@ RSpec.describe "Assignment API" do
                             outdoor: true
                           })
 
-      assignment_info = { assignment: {
+        assignment_info = { assignment: {
                             completed: true,
                             roommate_id: roommate.id,
                             chore_id: chore.id
                         } }
 
-      response = AssignmentService.create_assignment(assignment_info)
+        response = AssignmentService.create_assignment(assignment_info)
    
-      expect(response).to eq(Hash)
+        expect(response).to eq(Hash)
+      end
     end
   end
 end
