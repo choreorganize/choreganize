e47c937a9ca8355c98e0fd55e03b59632baa2359
Emmy Morris <emmymorris1102@gmail.com>
2021-08-03 18:46:40 -0600

    Add attributes to user poro

--- app/poros/google_user.rb
+++ app/poros/google_user.rb
@@ -3,7 +3,9 @@ class GoogleUser
               :name,
               :email,
               :household_id,
-              :token
+              :token,
+              :incomplete_chores,
+              :completed_chores
 
   def initialize(attributes)
     @google_id = attributes[:google_id]
@@ -11,5 +13,7 @@ class GoogleUser
     @email = attributes[:email]
     @household_id = attributes[:household_id]
     @token = attributes[:token]
+    @incomplete_chores = attributes[:incomplete_chores]
+    @completed_chores = attributes[:completed_chores]
   end
 end
--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "dashboard/user_show.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  
 end
