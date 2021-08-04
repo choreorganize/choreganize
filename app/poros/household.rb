d60c6db6743b110e6e93092eda16c4df2340dc71
Zach T <ztrokey@gmail.com>
2021-08-03 19:53:21 -0600

    update attributes

--- app/poros/household.rb
+++ app/poros/household.rb
@@ -1,11 +1,13 @@
 class Household
   attr_reader :address,
               :city,
-              :state
+              :state,
+              :id
 
   def initialize(attributes)
     @address = attributes[:address]
     @city = attributes[:city]
     @state = attributes[:state]
+    @id = attributes[:id]
   end
 end
