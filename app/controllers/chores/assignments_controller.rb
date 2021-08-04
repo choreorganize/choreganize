9a5085b96aaa974b68a5258e750907e067ba930e
Suzie K <suziekim.dev@gmail.com>
2021-08-04 10:28:38 -0400

    [ddd] Chore::AssignmentsController create action

--- app/controllers/chores/assignments_controller.rb
+++ app/controllers/chores/assignments_controller.rb
@@ -1,5 +1,14 @@
 class Chores::AssignmentsController < ApplicationController
   def create
-    
+    assignment = assignment_params 
+    assignment[:roomate_id] = current_user.id 
+    assignemtn[:chore_id] = params[:id] #double check this when chore show is set up 
+    AssignmentService.create_assignment(assignment_params)
+    TasksFacade.create_task(params[:chore_name], current_user) # add form to take in chore_name 
   end
+
+  private 
+    def assignment_params
+      params.permit(:roommate_id, :chore_id)
+    end
 end
