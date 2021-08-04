3be3a64f45a92e4731ef1a4c947eed2e51338eac
Zach T <ztrokey@gmail.com>
2021-08-04 11:45:34 -0600

    add daily forecast

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -4,8 +4,8 @@ class ChoresController < ApplicationController
   def show
     chore_id = params[:id].to_i
     @chore = current_user.incomplete_chores.select { |chore| chore.id == chore_id }.pop
-    require 'pry'; binding.pry
-    # @weather_forecast =
+    house = HouseholdsFacade.get_weather(current_user.household_id)
+    @daily_forecast = house.weather_forecast[:daily_weather]
   end
 end
 # GET /api/v1/household/:id
