c630f80d126dd26e27f4b2805f9b6e064ed00937
Zach T <ztrokey@gmail.com>
2021-08-04 10:35:03 -0600

    move conditional to poro

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -4,11 +4,8 @@ class ChoresController < ApplicationController
   def show
     chore_id = params[:id].to_i
     @chore = current_user.incomplete_chores.select { |chore| chore.id == chore_id }.pop
-    @location = if @chore.outdoor == true
-                  'Outdoor'
-                else
-                  'Indoor'
-                end
+    require 'pry'; binding.pry
+    # @weather_forecast =
   end
 end
 # GET /api/v1/household/:id
