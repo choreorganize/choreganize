c1ec382b54bf5f3d796d37854d0f3ab12734fbc9
Zach T <ztrokey@gmail.com>
2021-08-04 14:40:57 -0600

    add update method to chores

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -4,8 +4,14 @@ class ChoresController < ApplicationController
   def show
     chore_id = params[:id].to_i
     @chore = current_user.incomplete_chores.select { |chore| chore.id == chore_id }.pop
-    house = HouseholdsFacade.get_weather(current_user.household_id)
-    @daily_forecast = house.weather_forecast[:daily_weather]
+    @house = HouseholdsFacade.get_weather(current_user.household_id)
+    @daily_forecast = @house.weather_forecast[:daily_weather]
+  end
+
+  def update
+    # require 'pry'; binding.pry
+    # params[:household_id]
+    # params[:id]
   end
 end
 # GET /api/v1/household/:id
