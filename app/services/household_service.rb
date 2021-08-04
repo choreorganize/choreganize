56f4971b35e6742e26d631ea72c7ef17d5e584e5
Zach T <ztrokey@gmail.com>
2021-08-04 15:44:13 -0600

    remove variable assignment

--- app/services/household_service.rb
+++ app/services/household_service.rb
@@ -10,7 +10,6 @@ class HouseholdService
 
   def self.weather_data(household_id)
     response = connection.get("/api/v1/household/#{household_id}")
-    a = JSON.parse(response.body, symbolize_names: true)
-    # require 'pry'; binding.pry
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
