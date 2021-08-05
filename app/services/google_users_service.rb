9b736b6cba8794f947bdc2aaec95e8939fb8fe16
Zach T <ztrokey@gmail.com>
2021-08-04 22:19:46 -0600

    add update_user

--- app/services/google_users_service.rb
+++ app/services/google_users_service.rb
@@ -4,7 +4,13 @@ class GoogleUsersService
   end
 
   def self.create_or_find_user(user_info)
+    require 'pry'; binding.pry
     response = connection.post('/api/v1/roommates', user_info)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.update_user(user_info)
+    response = connection.patch("/api/v1/roommates/#{user_info[:roommate][:id]}", user_info)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
