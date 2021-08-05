3440e95e6822f9af5de4a1ec9d6eb777bb6295b9
Zach T <ztrokey@gmail.com>
2021-08-04 20:43:54 -0600

    updated poro to match kevins

--- app/poros/chore.rb
+++ app/poros/chore.rb
@@ -1,6 +1,5 @@
 class Chore
-  attr_reader :id,
-              :household_id,
+  attr_reader :household_id,
               :task_name,
               :description,
               :weight,
@@ -9,30 +8,18 @@ class Chore
               :id,
               :location
 
-  def initialize(attributes)
-    @id = attributes[:id]
-    @household_id = attributes[:household_id]
-    @task_name = attributes[:task_name]
-    @description = attributes[:description]
-    @weight = attributes[:weight]
-    @frequency = attributes[:frequency]
-    @outdoor = attributes[:outdoor]
-    @id = attributes[:id]
+  def initialize(chore_hash)
+    @household_id = chore_hash[:data][:attributes][:household_id].to_i
+    @task_name = chore_hash[:data][:attributes][:task_name]
+    @description = chore_hash[:data][:attributes][:description]
+    @weight = chore_hash[:data][:attributes][:weight]
+    @frequency = chore_hash[:data][:attributes][:frequency].to_i
+    @outdoor = chore_hash[:data][:attributes][:outdoor]
+    @id = chore_hash[:data][:id].to_i
   end
 
   def location
-    if @outdoor == true
-      'Outdoor'
-    else
-      'Indoor'
-    end
-  end
-
-  def location
-    if @outdoor == true
-      'Outdoor'
-    else
-      'Indoor'
-    end
+    'Indoor'
+    return 'Outdoor' if @outdoor == true
   end
 end
