3092f4915fb8c00bd5438c64e485f3832dc6c706
Zach T <ztrokey@gmail.com>
2021-08-04 22:19:07 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :dashboard, only: :index, as: 'user_dashboard'
-  resources :google_user, only: :create
+  resources :dashboard, only: :update, as: 'join_house'
 
   resources :households, only: %i[new create show] do
     resources :chores, only: %i[new create show update]
