89b611c9558a3e6a821e3d90cf115bef778647fd
Zach T <ztrokey@gmail.com>
2021-08-05 16:23:58 -0600

    remove pry

--- app/services/google_users_service.rb
+++ app/services/google_users_service.rb
@@ -9,7 +9,6 @@ class GoogleUsersService
   end
 
   def self.update_user(user_info)
-    # require 'pry'; binding.pry
     response = connection.patch("/api/v1/roommates/#{user_info[:roommate][:id]}", user_info)
     JSON.parse(response.body, symbolize_names: true)
   end
