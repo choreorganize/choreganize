ec32c33f08e4ff27592795bef65ce8ab729c2e4e
Zach T <ztrokey@gmail.com>
2021-08-05 16:23:32 -0600

    remove whitespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,10 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    
     user_info = {
       roommate:
       {
-        name: auth_hash['info']['name'], 
+        name: auth_hash['info']['name'],
         email: auth_hash['info']['email'],
         google_id: auth_hash['uid'],
         token: auth_hash['credentials']['token']
