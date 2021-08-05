0a3d895f19e7e7ba101e86bb4ee7e0b797a5bd59
Zach T <ztrokey@gmail.com>
2021-08-04 22:20:26 -0600

    add update method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,20 @@ class DashboardController < ApplicationController
     @incomplete_chores = @user.incomplete_chores
     @completed_chores = @user.completed_chores
   end
+
+  def update
+    # require 'pry'; binding.pry
+    update_info = {
+      roommate: {
+        id: params[:id],
+        household_id: params[:household_id]
+      }
+    }
+    GoogleUserFacade.update_user(update_info)
+    # params[:id] #user id
+    # params[:household_id]
+    # params[:house_password] <~~~Can we move this beyond our MVP?
+    redirect_to user_dashboard_index_path
+  end
 end
+# PATCH /api/v1/roommates/:id
