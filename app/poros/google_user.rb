d0bfcd0d4cbd5d03eefe8a15492c7f1ad62737e4
Zach T <ztrokey@gmail.com>
2021-08-05 16:23:44 -0600

    remove whitespace

--- app/poros/google_user.rb
+++ app/poros/google_user.rb
@@ -13,11 +13,9 @@ class GoogleUser
     @google_id = user_info[:data][:attributes][:google_id]
     @name = user_info[:data][:attributes][:name]
     @email = user_info[:data][:attributes][:email]
-    @household_id = user_info[:data][:attributes][:household_id]
+    @household_id = user_info[:data][:attributes][:household]
     @token = user_info[:data][:attributes][:token]
     @incomplete_chores = user_info[:data][:attributes][:incomplete_chores]
     @completed_chores = user_info[:data][:attributes][:completed_chores]
   end
 end
-
-
