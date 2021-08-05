6e8188e3c5469853aa3445489431beacf716cdb7
Zach T <ztrokey@gmail.com>
2021-08-04 22:20:11 -0600

    add update_user

--- app/facades/google_user_facade.rb
+++ app/facades/google_user_facade.rb
@@ -1,7 +1,13 @@
 class GoogleUserFacade
   def self.user(user_info)
+    require 'pry'; binding.pry
     user_attributes = GoogleUsersService.create_or_find_user(user_info)
     formatted = user_attributes[:data][:attributes]
     GoogleUser.new(formatted)
+  end
+
+  def self.update_user(user_info)
+    user_attributes = GoogleUsersService.update_user(user_info)
+    GoogleUser.new(user_attributes)
   end
 end
